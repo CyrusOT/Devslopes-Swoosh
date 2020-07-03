209b879868f07184dba498811f9f77ae7ce2b9bc
AhmedOT <iAhmedOT@gmail.com>
2020-07-03 13:00:49 -0700

    - Added a model.

---
+++ Swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  Swoosh
+//
+//  Created by Ahmed AlOtaibi on 7/3/20.
+//  Copyright © 2020 Ahmed AlOtaibi. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+}
