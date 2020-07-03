0b2ed2e1641a35a6ba079ab10d6ff3638f97829e
AhmedOT <iAhmedOT@gmail.com>
2020-07-02 19:46:11 -0700

    - Now works beautifully on both iPhone & iPad with different screen sizes (Thanks to Auto layout). - Updated the code to work with the latest Swift version (5.2). - cleanup code.

--- Swoosh/ViewController.swift
+++ Swoosh/ViewController.swift
@@ -10,9 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // IBOutlets
+    @IBOutlet weak var Swoosh: UIImageView!
+    @IBOutlet weak var BgImg: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
